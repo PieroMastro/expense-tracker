85f327b89959d3bf4b5ffd30ff68c5c53bfd87df
PieroMastro <pier.andres@gmail.com>
2023-05-05 03:33:26 +0000

    form created

---
+++ react-app/src/components/Form.tsx
@@ -0,0 +1,30 @@
+import React, { FormEvent } from "react";
+
+const handleSubmit = (event: FormEvent) => {
+  event.preventDefault();
+  console.log("Submitted");
+};
+
+const Form = () => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="from-label">
+          Age
+        </label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
