ded5564ccecc604d1c1c2be3860cfed72343f5f5
PieroMastro <pier.andres@gmail.com>
2023-05-07 06:47:54 +0000

    expenselist component created

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
-import Form from "./components/Form";
+// import Form3 from "./components/Form3";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "apple", amount: 10, category: "Fruits" },
+    { id: 2, description: "banana", amount: 10, category: "Fruits" },
+    { id: 3, description: "coconut", amount: 10, category: "Fruits" },
+  ]);
+
   return (
     <>
       <div>
-        <Form />
+        <ExpenseList
+          expenses={expenses}
+          onDelete={(id) =>
+            setExpenses(expenses.filter((event) => event.id !== id))
+          }
+        />
       </div>
     </>
   );
